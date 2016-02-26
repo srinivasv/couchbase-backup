14ea9353eea046de7713beaa7186cbd3973d3fd7
Srinivas Vadlamani <svadlamani@talena-inc.com>
2016-02-25 18:36:01 -0800

    tmp

--- src/main/scala/com/talena/agents/couchbase/compactor/Compactor.scala
+++ src/main/scala/com/talena/agents/couchbase/compactor/Compactor.scala
@@ -4,10 +4,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, SQLContext}
 
 class Compactor(sqlCtx: SQLContext, l0: L0, l1: L1, l2: L2) {
-  def removeDuplicatesInFilter(mutationStore: MutationStore) = {
+  def removeDuplicatesInFilter(mstore: MStore) = {
     import sqlCtx.implicits._
-    mutationStore match {
-      case L0(DataFramePair(_, filter), props) => filter
+    mstore match {
+      case L0(_, _, MData(_, Filter(filter, _, _), _)) => filter
         .groupBy($"pid", $"key")
         .max("seqno")
         .toDF("pid", "key", "seqno")
--- src/main/scala/com/talena/agents/couchbase/compactor/CompactorApp.scala
+++ src/main/scala/com/talena/agents/couchbase/compactor/CompactorApp.scala
@@ -8,64 +8,64 @@ import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
 
-case class Mutation(pid: Int, seqno: Long, key: String, value: String,
-  meta: String, data: String)
-case class Filter(pid: Int, seqno: Long, key: String)
-
-abstract class File
-case object MutationFile extends File
-case object FilterFile extends File
-
 object CompactorApp extends App {
-  def logError(msg: String) = {
+  def logError(msg: String): Unit = {
     System.err.println(msg)
   }
 
-  def parseJson(file: String) = {
-    import sqlCtx.implicits._
-    val spec = sqlCtx.read.json(file)
+  def parseJson(file: String): List[LevelProps] = {
+    import sqlContext.implicits._
+    val props = sqlContext.read.json(file)
     def v(row: Row, col: String) = row(row.fieldIndex(col))
 
-    val l0Row = spec.select("pset.id", "pset.l0.location", "pset.l0.format")
+    val l0Row = props.select("pset.id", "pset.l0.location", "pset.l0.format")
       .collect()(0)
-    val l0Props = MutationStoreProps(v(l0Row, "id").toString(), L0,
+    val l0Props = LevelProps(v(l0Row, "id").toString(), L0,
       v(l0Row, "location").toString(), CSV)
 
-    val l1Row = spec.select("pset.id", "pset.l1.location", "pset.l1.format")
+    val l1Row = props.select("pset.id", "pset.l1.location", "pset.l1.format")
       .collect()(0)
-    val l1Props = MutationStoreProps(v(l1Row, "id").toString(), L1,
+    val l1Props = LevelProps(v(l1Row, "id").toString(), L1,
       v(l1Row, "location").toString(), CSV)
 
-    val l2Row = spec.select("pset.id", "pset.l2.location", "pset.l2.format")
+    val l2Row = props.select("pset.id", "pset.l2.location", "pset.l2.format")
       .collect()(0)
-    val l2Props = MutationStoreProps(v(l2Row, "id").toString(), L2,
+    val l2Props = LevelProps(v(l2Row, "id").toString(), L2,
       v(l2Row, "location").toString(), CSV)
 
     List(l0Props, l1Props, l2Props)
   }
 
-  def parseArgs() = {
+  def parseArgs(): Option[List[LevelProps]] = {
     args match {
       case Array(file) => Some(parseJson(file))
       case _ => None
     }
   }
 
-  def verifyLocation(fs: FileSystem, location: String) = {
+  def verifyLocation(location: String): Boolean = {
     val path = new Path(location)
-    if (!fs.exists(path))
+    if (!fileSystem.exists(path))
       throw new IllegalArgumentException(location + " does not exist")
-    if (!fs.getFileStatus(path).isDirectory)
+    if (!fileSystem.getFileStatus(path).isDirectory)
       throw new IllegalArgumentException(location + " is not a directory")
     true
   }
 
-  def verifyFiles(status: Boolean, fs: FileSystem, mutationFiles: String, filterFile: String) = {
-    val mutationsFilesExist = fs.exists(new Path(mutationFiles))
-    val filterFileExists = fs.exists(new Path(filterFile))
+  def verifyFiles(location: String, files: (String, String)): Boolean = {
+    verifyLocation(location)
+    val (mutationFiles, filterFile) = files
+    val mutationsFilesExist = (fileSystem.globStatus(new Path(mutationFiles))
+      .length > 0)
+    val filterFileExists = (fileSystem.globStatus(new Path(filterFile)).length
+      == 1)
     (mutationsFilesExist, filterFileExists) match {
-      case (true, true) => true
-      case (false, false) => false
+      case (true, true) =>
+        System.out.println("Found " + files);
+        true
+      case (false, false) =>
+        System.out.println(files + " missing");
+        false
       case (true, false) => throw new IllegalArgumentException(
         "Filter file " + filterFile + " missing")
       case (false, true) => throw new IllegalArgumentException(
@@ -73,67 +73,72 @@ object CompactorApp extends App {
     }
   }
 
-  def openDataFrame(files: String, fileType: File) = {
-    import sqlCtx.implicits._
-    val df = sparkCtx.textFile(files)
-    fileType match {
-      case MutationFile => df.map(_.split(",")).map(p => Mutation(
+  def getMutation(files: String, format: FileFormat): Mutation = {
+    import sqlContext.implicits._
+    val df = sparkContext.textFile(files)
+      .map(_.split(",")).map(p => MutationSchema(
         p(0).toInt, p(1).toInt, p(2), p(3), p(4), p(5))).toDF()
-      case FilterFile => df.map(_.split(",")).map(p => Filter(
+    Mutation(df, Array(files), format)
+  }
+
+  def getFilter(file: String, format: FileFormat): Filter = {
+    import sqlContext.implicits._
+    val df = sparkContext.textFile(file)
+      .map(_.split(",")).map(p => FilterSchema(
         p(0).toInt, p(1).toInt, p(2))).toDF()
+    Filter(df, file, format)
+  }
+
+  def getFailoverLog(): Option[FailoverLog] = {
+    None
+  }
+
+  def getMData(props: LevelProps): Option[MData] = {
+    val mutationFiles = props.location + "/*.db"
+    val filterFile = props.location + "/*.meta"
+    if(verifyFiles(props.location, (mutationFiles, filterFile))) {
+      Some(MData(
+        getMutation(mutationFiles, props.format),
+        getFilter(filterFile, props.format),
+        getFailoverLog()))
+    } else {
+      None
     }
   }
 
-  def openDataFramePair(status: Boolean, mutationFiles: String,
-    filterFile: String, format: FileFormat) = {
-
-    status match {
-     case true => Some(DataFramePair(
-      openDataFrame(mutationFiles, MutationFile),
-      openDataFrame(filterFile, FilterFile)))
-     case false => None
-    }
-  }
-
-  def openMutationStore(props: MutationStoreProps) = {
+  def openMStore(props: LevelProps): MStore = {
     println("Initializing props: " + props)
-    val fs = FileSystem.get(sparkCtx.hadoopConfiguration)
-    val mutationFiles = props.location + "*.db"
-    val filterFile = props.location + "*.meta"
-    val dataFramePair = Try(verifyLocation(fs, props.location))
-      .map(status => verifyFiles(status, fs, mutationFiles, filterFile))
-      .map(status => openDataFramePair(status, mutationFiles, filterFile,
-        props.format))
-    (props.level, dataFramePair) match {
-      case (L0, Success(Some(pair))) => L0(pair, props)
-      case (L0, Success(None)) => throw new IllegalArgumentException(
-        "L0 location may not be empty")
-      case (L1, Success(Some(pair))) => L1(Some(pair), props)
-      case (L1, Success(None)) => L1(None, props)
-      case (L2, Success(Some(pair))) => L2(Some(pair), props)
-      case (L2, Success(None)) => L2(None, props)
-      case (_, Failure(ex)) => ex
+    (props.level, getMData(props))  match {
+      case (L0, Some(mdata)) => L0(props.psid, props.location, mdata)
+      case (L0, None) => throw new IllegalArgumentException(
+        "L0 location may not be empty");
+      case (L1, mdata) => L1(props.psid, props.location, mdata)
+      case (L2, mdata) => L2(props.psid, props.location, mdata)
     }
   }
 
-  def openMutationStores() = {
+  def openMStores(): List[MStore] = {
     parseArgs() match {
-      case Some(l) => l.map(openMutationStore)
+      case Some(l) => l.map(openMStore)
       case None => throw new IllegalArgumentException(
         "JSON file path argument missing")
     }
   }
 
   val conf = new SparkConf().setAppName("Couchbase Compactor")
-  val sparkCtx = new SparkContext(conf)
-  val sqlCtx = new SQLContext(sparkCtx)
+  val sparkContext = new SparkContext(conf)
+  val sqlContext = new SQLContext(sparkContext)
+  val fileSystem = FileSystem.get(sparkContext.hadoopConfiguration)
 
-  val (l0: L0, l1: L1, l2: L2) = openMutationStores() match {
+  val (l0: L0, l1: L1, l2: L2) = openMStores() match {
     case List(l0: L0, l1: L1, l2: L2) => (l0, l1, l2)
   }
+  System.out.println(l0)
+  System.out.println(l1)
+  System.out.println(l2)
 
-  val compactor = new Compactor(sqlCtx, l0, l1, l2)
+  val compactor = new Compactor(sqlContext, l0, l1, l2)
   compactor.compact()
 
-  sparkCtx.stop()
+  sparkContext.stop()
 }
